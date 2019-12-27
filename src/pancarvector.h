2d6d164efeba702c18c39c2b4680f8e2f97d2e1a
thomasarmel <24313857+thomasarmel@users.noreply.github.com>
2019-12-27 19:53:45 +0100

    It works :-)

--- src/pancarvector.h
+++ src/pancarvector.h
@@ -1,6 +1,11 @@
 #ifndef PANCARVECTOR_H_INCLUDED
 #define PANCARVECTOR_H_INCLUDED
 
+/**
+    NOTE AUX CORRECTEURS:
+    J'explique ce code dans un pdf, dossier datasheets
+*/
+
 #if ARDUINO >= 100
   #include "Arduino.h"
 #else
@@ -9,9 +14,9 @@
   #include "WConstants.h"
 #endif
 
-namespace pancar
+namespace pancar /// Not really RAM-friendly at this time, must be improved
 {
-    template <typename T>
+template <typename T>
     class Vector
     {
     public:
@@ -20,32 +25,36 @@ namespace pancar
         unsigned int size() const;
         void add(const T &element);
         T &get(unsigned int index) const;
+        T &operator[](unsigned int index) const;
+        void operator+=(Vector<T> v);
+        void operator+=(const T &element);
+        bool remove(unsigned int index);
 
-    private:
+    protected:
         unsigned int m_size;
         void *m_firstElementArray[2];
     };
 
-    template<typename T>
+    template <typename T>
     Vector<T>::Vector() : m_size(0)
     {}
 
-    template<typename T>
+    template <typename T>
     Vector<T>::~Vector()
     {}
 
-    template<typename T>
+    template <typename T>
     unsigned int Vector<T>::size() const
     {
         return m_size;
     }
 
-    template<typename T>
+    template <typename T>
     void Vector<T>::add(const T &element)
     {
         if(m_size==0)
         {
-            m_firstElementArray[0]=&element;
+            m_firstElementArray[0]=(void*)&element;
             m_firstElementArray[1]=0;
         }
         else
@@ -55,12 +64,10 @@ namespace pancar
             {
                 pointerNext=(*((void*(*)[2])pointerNext))[1];
             }
-            Serial.println((*((T*)((*((void*(*)[2])pointerNext))[0]))));
-            void *elementArray[2]={&element, 0};
-            (*((void*(*)[2])pointerNext))[1]=&elementArray;
-            void *p=&elementArray;
-            Serial.println((int)p);
-            Serial.println(*((T*)(*((void*(*)[2])p))[0]));
+            void **elementArray=(void**)malloc(sizeof(void*) * 2);
+            elementArray[0]=(void*)&element;
+            elementArray[1]=0;
+            (*((void*(*)[2])pointerNext))[1]=(void*)elementArray;
         }
         m_size++;
     }
@@ -72,15 +79,73 @@ namespace pancar
         {
             return *((T*)(m_firstElementArray[0]));
         }
-        void *pointerNext=&m_firstElementArray;
-        for(unsigned int i=0; i<(m_size-1); i++)
+        void *pointerNext=m_firstElementArray[1];
+        for(unsigned int i=0; i<(index-1); i++)
         {
             pointerNext=(*((void*(*)[2])pointerNext))[1];
         }
-        void *p=pointerNext;
-        Serial.println((int)p);
-        Serial.println(*((T*)(*((void*(*)[2])p))[0]));
-        return *((T*)(*((void*(*)[2])p))[0]);
+        return *((T*)(*((void*(*)[2])pointerNext))[0]);
+    }
+
+    template<typename T>
+    T &Vector<T>::operator[](unsigned int index) const
+    {
+        return get(index);
+    }
+
+    template<typename T>
+    void Vector<T>::operator+=(Vector<T> v)
+    {
+        for(unsigned int i=0; i<v.size(); i++)
+        {
+            add(v.get(i));
+        }
+    }
+
+    template<typename T>
+    void Vector<T>::operator+=(const T &element)
+    {
+        add(element);
+    }
+
+    template<typename T>
+    bool Vector<T>::remove(unsigned int index)
+    {
+        if(m_size<=index)
+        {
+            return false;
+        }
+        if(index==0)
+        {
+            m_firstElementArray[0]=(*((void*(*)[2])m_firstElementArray[1]))[0];
+            m_firstElementArray[1]=(*((void*(*)[2])m_firstElementArray[1]))[1];
+        }
+        else
+        {
+            void *pointerNext=&m_firstElementArray;
+            for(unsigned int i=0; i<(index-1); i++)
+            {
+                pointerNext=(*((void*(*)[2])pointerNext))[1];
+            }
+            (*((void*(*)[2])pointerNext))[1]=(*((void*(*)[2]) (*((void*(*)[2])pointerNext))[1] ))[1];
+        }
+        m_size--;
+        return true;
+    }
+
+    template<typename T>
+    Vector<T> operator+(Vector<T> const& a, Vector<T> const& b)
+    {
+        Vector<T> v;
+        for(unsigned int i=0; i<a.size(); i++)
+        {
+            v.add(a.get(i));
+        }
+        for(unsigned int i=0; i<b.size(); i++)
+        {
+            v.add(b.get(i));
+        }
+        return v;
     }
 }
 
