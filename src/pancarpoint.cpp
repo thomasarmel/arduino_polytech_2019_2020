c97cbcbf6ab9652efcde5fe3779fbb86ddc42357
clarachr <58274233+clarachr@users.noreply.github.com>
2020-01-07 13:55:41 +0100

    Create pancarpoint.cpp

---
+++ src/pancarpoint.cpp
@@ -0,0 +1,26 @@
+#include "pancarpoint.h"
+
+using namespace pancar ;
+
+
+Point::Point() : m_x(0), m_y(0) {}
+
+Point::Point(unsigned short x, unsigned short y) : m_x(x), m_y(y){}
+
+Point::~Point(){}
+
+void Point::setX(unsigned short x) 
+{
+	m_x=x;
+}
+void Point::setY(unsigned short y) 
+{
+	m_y=y;
+}
+
+unsigned short Point::getX(){ 
+	return m_x ; 
+}
+unsigned short Point::getY(){ 
+	return m_y;
+}
