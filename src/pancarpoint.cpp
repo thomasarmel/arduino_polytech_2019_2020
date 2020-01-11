a0745a96ce4ca52c8fd0f4c68f9e31535fc02d14
thomasarmel <24313857+thomasarmel@users.noreply.github.com>
2020-01-11 15:22:58 +0100

    Update pancarpoint.cpp

--- src/pancarpoint.cpp
+++ src/pancarpoint.cpp
@@ -1,26 +1,32 @@
 #include "pancarpoint.h"
 
-using namespace pancar ;
+using namespace pancar;
 
+Point::Point() : m_x(0), m_y(0)
+{}
 
-Point::Point() : m_x(0), m_y(0) {}
+Point::Point(unsigned short x, unsigned short y) : m_x(x), m_y(y)
+{}
 
-Point::Point(unsigned short x, unsigned short y) : m_x(x), m_y(y){}
-
-Point::~Point(){}
+Point::~Point()
+{}
 
 void Point::setX(unsigned short x) 
 {
 	m_x=x;
 }
+
 void Point::setY(unsigned short y) 
 {
 	m_y=y;
 }
 
-unsigned short Point::getX(){ 
+unsigned short Point::getX() const
+{ 
 	return m_x ; 
 }
-unsigned short Point::getY(){ 
+
+unsigned short Point::getY() const
+{ 
 	return m_y;
 }
